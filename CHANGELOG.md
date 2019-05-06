487d50020bf1fd26d55b48a8983a6e35380beb7a
Alibaba Cloud SDK <sdk-team@alibabacloud.com>
2019-05-06 09:25:10 +0800

    Generated 2014-07-14 for Bss

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.5.7 - 2019-5-6
+- Generated 2014-07-14 for `Bss`
+
+
 ## 1.5.6 - 2019-5-6
 - Generated 2015-02-27 for `Yundun`
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.5.6';
+    const VERSION = '1.5.7';
 
     /**
      * @param Event $event
