b1bfe6bc71766b6bd5a458843f6846901cb2ff4a
sdk-team <sdk-team@alibabacloud.com>
2019-05-28 15:12:36 +0800

    测试发布云盾 1.0.1

--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.5.99';
+    const VERSION = '1.5.100';
 
     /**
      * @param Event $event
--- src/Yundun/V20150227/YundunApiResolver.php
+++ src/Yundun/V20150227/YundunApiResolver.php
@@ -6,15 +6,15 @@ use AlibabaCloud\Client\Resolver\ApiResolver;
 
 /**
  * @method AllMalwareNum allMalwareNum(array $options = [])
- * @method TodayAegisOnlineRate todayAegisOnlineRate(array $options = [])
- * @method TodayMalwareNum todayMalwareNum(array $options = [])
- * @method TodayCrackIntercept todayCrackIntercept(array $options = [])
- * @method CurrentDdosAttackNum currentDdosAttackNum(array $options = [])
- * @method TodayAllkbps todayAllkbps(array $options = [])
- * @method TodayBackdoor todayBackdoor(array $options = [])
  * @method TodayAllpps todayAllpps(array $options = [])
  * @method TodayqpsByRegion todayqpsByRegion(array $options = [])
+ * @method TodayCrackIntercept todayCrackIntercept(array $options = [])
  * @method WebAttackNum webAttackNum(array $options = [])
+ * @method TodayMalwareNum todayMalwareNum(array $options = [])
+ * @method TodayAegisOnlineRate todayAegisOnlineRate(array $options = [])
+ * @method TodayAllkbps todayAllkbps(array $options = [])
+ * @method CurrentDdosAttackNum currentDdosAttackNum(array $options = [])
+ * @method TodayBackdoor todayBackdoor(array $options = [])
  */
 class YundunApiResolver extends ApiResolver
 {
@@ -39,30 +39,6 @@ class AllMalwareNum extends Rpc
 {
 }
 
-class TodayAegisOnlineRate extends Rpc
-{
-}
-
-class TodayMalwareNum extends Rpc
-{
-}
-
-class TodayCrackIntercept extends Rpc
-{
-}
-
-class CurrentDdosAttackNum extends Rpc
-{
-}
-
-class TodayAllkbps extends Rpc
-{
-}
-
-class TodayBackdoor extends Rpc
-{
-}
-
 class TodayAllpps extends Rpc
 {
 }
@@ -71,6 +47,30 @@ class TodayqpsByRegion extends Rpc
 {
 }
 
+class TodayCrackIntercept extends Rpc
+{
+}
+
 class WebAttackNum extends Rpc
+{
+}
+
+class TodayMalwareNum extends Rpc
+{
+}
+
+class TodayAegisOnlineRate extends Rpc
+{
+}
+
+class TodayAllkbps extends Rpc
+{
+}
+
+class CurrentDdosAttackNum extends Rpc
+{
+}
+
+class TodayBackdoor extends Rpc
 {
 }
