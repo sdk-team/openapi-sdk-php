f3d00a6921636d09b128f84c50a7d154b1c887a6
sdk-team <sdk-team@alibabacloud.com>
2019-05-31 10:11:37 +0800

    Generated 2015-05-06 for cr

--- src/Cr/V20150506/CrApiResolver.php
+++ src/Cr/V20150506/CrApiResolver.php
@@ -5,8 +5,8 @@ namespace AlibabaCloud\Cr\V20150506;
 use AlibabaCloud\Client\Resolver\ApiResolver;
 
 /**
- * @method DoCheckResource doCheckResource(array $options = [])
  * @method DoPhysicalDeleteResource doPhysicalDeleteResource(array $options = [])
+ * @method DoCheckResource doCheckResource(array $options = [])
  * @method DoLogicalDeleteResource doLogicalDeleteResource(array $options = [])
  */
 class CrApiResolver extends ApiResolver
@@ -26,6 +26,34 @@ class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
 
     /** @var string */
     public $serviceCode = 'cr';
+}
+
+/**
+ * @method string getCountry()
+ * @method $this withCountry($value)
+ * @method string getHid()
+ * @method $this withHid($value)
+ * @method string getInvoker()
+ * @method $this withInvoker($value)
+ * @method string getMessage()
+ * @method $this withMessage($value)
+ * @method string getSuccess()
+ * @method $this withSuccess($value)
+ * @method string getInterrupt()
+ * @method $this withInterrupt($value)
+ * @method string getGmtWakeup()
+ * @method $this withGmtWakeup($value)
+ * @method string getPk()
+ * @method $this withPk($value)
+ * @method string getBid()
+ * @method $this withBid($value)
+ * @method string getTaskExtraData()
+ * @method $this withTaskExtraData($value)
+ * @method string getTaskIdentifier()
+ * @method $this withTaskIdentifier($value)
+ */
+class DoPhysicalDeleteResource extends Rpc
+{
 }
 
 /**
@@ -59,34 +87,6 @@ class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
  * @method $this withTaskIdentifier($value)
  */
 class DoCheckResource extends Rpc
-{
-}
-
-/**
- * @method string getCountry()
- * @method $this withCountry($value)
- * @method string getHid()
- * @method $this withHid($value)
- * @method string getInvoker()
- * @method $this withInvoker($value)
- * @method string getMessage()
- * @method $this withMessage($value)
- * @method string getSuccess()
- * @method $this withSuccess($value)
- * @method string getInterrupt()
- * @method $this withInterrupt($value)
- * @method string getGmtWakeup()
- * @method $this withGmtWakeup($value)
- * @method string getPk()
- * @method $this withPk($value)
- * @method string getBid()
- * @method $this withBid($value)
- * @method string getTaskExtraData()
- * @method $this withTaskExtraData($value)
- * @method string getTaskIdentifier()
- * @method $this withTaskIdentifier($value)
- */
-class DoPhysicalDeleteResource extends Rpc
 {
 }
 
