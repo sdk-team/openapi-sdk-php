6c73981077aa1632e2dd87c51d3cdc0f5447981f
sdk-team <sdk-team@alibabacloud.com>
2019-06-18 20:40:43 +0800

    * 测试八种语言 SDK 发布

--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.5.148';
+    const VERSION = '1.5.149';
 
     /**
      * @param Event $event
--- src/publish.sh
+++ src/publish.sh
@@ -4,3 +4,4 @@ java -jar /Users/yijun/git/portal/tools/sdk.generator.jar Aas 2015-07-01 PhpWith
 java -jar /Users/yijun/git/portal/tools/sdk.generator.jar Aas 2015-07-01 PhpWithComposer aas public /Users/yijun/git/portal/output/composer/src /Users/yijun/git/portal/tools/pop_config.properties ""  test openAPI
 java -jar /Users/yijun/git/portal/tools/sdk.generator.jar Aas 2015-07-01 PhpWithComposer aas public /Users/yijun/git/portal/output/composer/src /Users/yijun/git/portal/tools/pop_config.properties ""  test openAPI
 java -jar /Users/yijun/git/portal/tools/sdk.generator.jar Aas 2015-07-01 PhpWithComposer aas public /Users/yijun/git/portal/output/composer/src /Users/yijun/git/portal/tools/pop_config.properties ""  test openAPI
+java -jar /Users/yijun/git/portal/tools/sdk.generator.jar Aas 2015-07-01 PhpWithComposer aas public /Users/yijun/git/portal/output/composer/src /Users/yijun/git/portal/tools/pop_config.properties ""  test openAPI
