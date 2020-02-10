bd11c787d4bb5344232663c9237d777a44dc16a0
sdk-team <sdk-team@alibabacloud.com>
2020-02-11 00:50:49 +0800

    Generated 2019-06-20 for EcsDemo.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.5.176 - 2020-2-11
+- Generated 2019-06-20 for `EcsDemo`.
+
+
 ## 1.5.175 - 2020-2-11
 - Generated 2019-06-20 for `EcsDemo`.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.5.175';
+    const VERSION = '1.5.176';
 
     /**
      * @param Event $event
