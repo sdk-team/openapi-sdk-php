8fec518b352da32c35f2b958980bbd843810f94f
sdk-team <sdk-team@alibabacloud.com>
2020-05-26 20:42:04 +0800

    Generated 2015-09-01 for ROS.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.5.225 - 2020-5-26
+- Generated 2015-09-01 for `ROS`.
+
+
 ## 1.5.224 - 2020-5-19
 - Generated 2017-03-21 for `vod`.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.5.224';
+    const VERSION = '1.5.225';
 
     /**
      * @param Event $event
