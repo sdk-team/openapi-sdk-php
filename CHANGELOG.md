4c692383db0606fd3f80e77dcc8039f4710ce938
sdk-team <sdk-team@alibabacloud.com>
2020-08-20 16:13:23 +0000

    Generated 2018-07-13 for Ft.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 1.5.301 - 2020-8-20
+- Generated 2018-07-13 for `Ft`.
+
+
 ## 1.5.300 - 2020-8-20
 - Generated 2019-02-25 for `sdk-test`.
 
--- src/Release.php
+++ src/Release.php
@@ -16,7 +16,7 @@ class Release
     /**
      * Version of the SDK
      */
-    const VERSION = '1.5.300';
+    const VERSION = '1.5.301';
 
     /**
      * @param Event $event
